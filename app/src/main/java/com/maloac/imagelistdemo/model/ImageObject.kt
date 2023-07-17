4d15d18f96497afb7fbe913e3f4b796c2c880f04
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-07-17 16:24:32 -0400

    Default value added to ImageObject.kt model

--- app/src/main/java/com/maloac/imagelistdemo/model/ImageObject.kt
+++ app/src/main/java/com/maloac/imagelistdemo/model/ImageObject.kt
@@ -2,5 +2,5 @@ package com.maloac.imagelistdemo.model
 
 data class ImageObject(
     val imageName: String? = "Name not available",
-    val imageUrl: String? = null,
+    val imageUrl: String? = "https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg",
 )
